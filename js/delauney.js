c3112abb7c1613dda11c5e75f43b30d6d45691ba
Adam Rohdin <adam.rohdin@outlook.com>
2018-11-15 12:21:21 +0100

    not optimized enough, sad

--- js/delauney.js
+++ js/delauney.js
@@ -1,8 +1,8 @@
 let _width, _height;
-let xmin, xmax, ymin, ymax;
+let xmin, xmax, ymin, ymax, zmin, zmax;
 
 //init function; creates square 
-function initDelaunay(dx2, dy2, he, vl, fl) {
+function initDelaunay(dx2, dy2, dz2, he, vl, fl) {
     
     _width = dx2*2;
     _height = dy2*2;
@@ -10,23 +10,28 @@ function initDelaunay(dx2, dy2, he, vl, fl) {
     xmax = dx2;
     ymin = -dy2;
     ymax = dy2;
+    zmin = -dz2;
+    zmax = dz2;
 
+    let v0 = new VertexNode(new THREE.Vector3(-dx2,dy2,0));
+    let v1 = new VertexNode(new THREE.Vector3(-dx2,-dy2,0));
+    let v2 = new VertexNode(new THREE.Vector3(dx2,dy2,0));
+    let v3 = new VertexNode(new THREE.Vector3(dx2,-dy2,0));
 
-    let v0 = new VertexNode(new THREE.Vector3(-dx2,dy2,0.0));
-    let v1 = new VertexNode(new THREE.Vector3(-dx2,-dy2,0.0));
-    let v2 = new VertexNode(new THREE.Vector3(dx2,dy2,0.0));
-    let v3 = new VertexNode(new THREE.Vector3(dx2,-dy2,0.0));
-
-    vl.push(v0);
-    vl.push(v1);
-    vl.push(v2);
-    vl.push(v3);
+    vl[v0.id] = v0;
+    vl[v1.id] = v1;
+    vl[v2.id] = v2;
+    vl[v3.id] = v3;
 
     let t1 = new Face();
     let t2 = new Face();
 
-    fl.push(t1);
-    fl.push(t2);
+    t1.color = {'r': Math.random(), 'g': Math.random(), 'b': Math.random()};
+    t2.color = {'r': Math.random(), 'g': Math.random(), 'b': Math.random()};
+
+
+    fl[t1.id] = t1;
+    fl[t2.id] = t2;
 
     let e0 = new HalfEdge(v0, t1);
     let e1 = new HalfEdge(v1, t1);
@@ -50,12 +55,12 @@ function initDelaunay(dx2, dy2, he, vl, fl) {
     e1.setTwin(e3);
     e3.setTwin(e1);
 
-    he.push(e0);
-    he.push(e1);
-    he.push(e2);
-    he.push(e3);
-    he.push(e4);
-    he.push(e5);
+    he[e0.id] = e0;
+    he[e1.id] = e1;
+    he[e2.id] = e2;
+    he[e3.id] = e3;
+    he[e4.id] = e4;
+    he[e5.id] = e5;
 
 }
 
@@ -63,8 +68,9 @@ function samplePosition() {
 
     let x = (Math.random() * (xmax-xmin)) + xmin;
     let y = (Math.random() * (ymax-ymin)) + ymin;
+    let z = (Math.random() * (zmax-zmin)) + zmin;
 
-    return new THREE.Vector3(x,y,0);
+    return new THREE.Vector3(x,y,z);
 }
 
 function sign(p1, p2, p3) {
@@ -89,14 +95,14 @@ function insideTriangle(point, face) {
 
 
 function insertPoint(point, faceList, edgeList, vertexList) {
-    
-    for(let i = 0; i  < faceList.length; ++i) {
-        let isInside = insideTriangle(point, faceList[i]);
+
+    for(let key in faceList) {
+        let isInside = insideTriangle(point, faceList[key]);  
         if(isInside) {
-            triangleSplit(point, i, faceList, edgeList, vertexList);
-            checkDelaunay(edgeList)
+            triangleSplit(point, key, faceList, edgeList, vertexList);
+            checkDelaunay(edgeList, faceList)
             return true;
-        }
+        } 
     }
     return false;
 
@@ -105,13 +111,18 @@ function insertPoint(point, faceList, edgeList, vertexList) {
 function triangleSplit(point, index, faceList, edgeList, vertexList) {
    
     let vertex = new VertexNode(point);
-    vertexList.push(vertex);
+    vertexList[vertex.id] = vertex;
    
     let face = faceList[index];
 
     let f1 = new Face();
     let f2 = new Face();
     let f3 = new Face();
+
+    f1.color = {'r': Math.random(), 'g': Math.random(), 'b': Math.random()};
+    f2.color = {'r': Math.random(), 'g': Math.random(), 'b': Math.random()};
+    f3.color = {'r': Math.random(), 'g': Math.random(), 'b': Math.random()};
+
 
     let e0 = new HalfEdge(face.edge.vertex,f3);
     let e1 = new HalfEdge(face.edge.next.vertex, f1);
@@ -134,6 +145,10 @@ function triangleSplit(point, index, faceList, edgeList, vertexList) {
     let ee0 = face.edge;
     let ee1 = face.edge.next;
     let ee2 = face.edge.prev;
+
+    ee0.face = f1;
+    ee1.face = f2;
+    ee2.face = f3;
 
     //face 3
     e0.next = e5;
@@ -159,27 +174,26 @@ function triangleSplit(point, index, faceList, edgeList, vertexList) {
     ee1.next = e2;
     ee1.prev = e4;
 
-    edgeList.push(e0);
-    edgeList.push(e1);
-    edgeList.push(e2);
-    edgeList.push(e3);
-    edgeList.push(e4);
-    edgeList.push(e5);
+    edgeList[e0.id] = e0;
+    edgeList[e1.id] = e1;
+    edgeList[e2.id] = e2;
+    edgeList[e3.id] = e3;
+    edgeList[e4.id] = e4;
+    edgeList[e5.id] = e5;
 
-    faceList.push(f1);
-    faceList.push(f2);
-    faceList.push(f3);
+    faceList[f1.id] = f1;
+    faceList[f2.id] = f2;
+    faceList[f3.id] = f3;
 
-    faceList.splice(index,1);
+    delete faceList[index];
 
 }
 
-function checkDelaunay(edgeList) {
-    for(let i = 0; i < edgeList.length; ++i) {
-        let b = isDelaunay(edgeList[i]);
+function checkDelaunay(edgeList, faceList) {
+    for(let key in edgeList) {
+        let b = isDelaunay(edgeList[key]);
         if(!b) {
-            flipEdge(edgeList[i], edgeList);
-            console.log("Flipped Edge");
+            flipEdge(key, edgeList, faceList);
         }
     }
 }
@@ -193,13 +207,13 @@ function isDelaunay(edge) {
     let v2 = edge.prev.vertex.point;
     let v3 = edge.twin.prev.vertex.point;
 
-    let v1v2 = v1.sub(v2);
-    let v0v2 = v0.sub(v2);
-    let v1v3 = v1.sub(v3);
-    let v0v3 = v0.sub(v3);
+    let v1v2 =  (new THREE.Vector3(v1.x-v2.x, v1.y-v2.y, v1.z-v2.z)).normalize();
+    let v0v2 =  (new THREE.Vector3(v0.x-v2.x, v0.y-v2.y, v0.z-v2.z)).normalize();
+    let v1v3 =  (new THREE.Vector3(v1.x-v3.x, v1.y-v3.y, v1.z-v3.z)).normalize();
+    let v0v3 =  (new THREE.Vector3(v0.x-v3.x, v0.y-v3.y, v0.z-v3.z)).normalize();
 
-    let alpha = v1v2.dot(v0v2);
-    let beta = v1v3.dot(v0v3);
+    let alpha = Math.acos(clamp(.1,1,v1v2.dot(v0v2)));
+    let beta = Math.acos(clamp(-1,1,v1v3.dot(v0v3)));
 
     if(alpha+beta > Math.PI)
         return false;
@@ -207,56 +221,184 @@ function isDelaunay(edge) {
     return true;
 }
 
-function flipEdge(edge, edgeList) {
-    let newEdge = new HalfEdge(edge.prev.vertex, edge.twin.face);
-    let newTwin = new HalfEdge(edge.twin.prev.vertex, edge.face);
 
-    newEdge.next = edge.twin.prev;
-    newEdge.prev = edge.next;
+function flipEdge(i, edgeList, faceList) {
+    //create 6 new edges
+    let e0 = new HalfEdge(edgeList[i].twin.prev.vertex, edgeList[i].face);
+    let e1 = new HalfEdge(edgeList[i].prev.vertex, edgeList[i].face);
+    let e2 = new HalfEdge(edgeList[i].vertex, edgeList[i].face);
+    let e3 = new HalfEdge(edgeList[i].prev.vertex, edgeList[i].twin.face);
+    let e4 = new HalfEdge(edgeList[i].twin.prev.vertex, edgeList[i].twin.face);
+    let e5 = new HalfEdge(edgeList[i].next.vertex, edgeList[i].twin.face);
 
-    newTwin.next = edge.prev;
-    newTwin.prev = edge.twin.next;
+    let edgeToRemove = [];
+    edgeToRemove.push(edgeList[i].id);
+    edgeToRemove.push(edgeList[i].next.id);
+    edgeToRemove.push(edgeList[i].prev.id);
+    edgeToRemove.push(edgeList[i].twin.id);
+    edgeToRemove.push(edgeList[i].twin.next.id);
+    edgeToRemove.push(edgeList[i].twin.prev.id);
 
-    edge.twin.prev.next = edge.next;
-    edge.twin.prev.prev = newEdge;
-    edge.next.prev = newEdge.next;
-    edge.next.next = newEdge;
-
-    edge.prev.next = edge.twin.next;
-    edge.prev.prev = newTwin;
-    edge.twin.next.next = newTwin;
-    edge.twin.next.prev = newTwin.next;
-
-    newEdge.setTwin(newTwin);
-    newTwin.setTwin(newEdge);
     
-    edge.face.edge = newEdge;
-    edge.twin.face.edge = newTwin;
+    //set face edge
+    edgeList[i].face.edge = e0;
+    edgeList[i].twin.face.edge = e3;
 
-    newEdge.prev.face = newEdge.face;
-    newTwin.prev.face = newTwin.face;
+    //save all connections
+    e0.next = e1;
+    e0.prev = e2;
+    e1.next = e2;
+    e1.prev = e0;
+    e2.next = e0;
+    e2.prev = e1;
 
-    edge = newEdge;
-    edge.twin = newTwin;
+    e3.next = e4;
+    e3.prev = e5;
+    e4.next = e5;
+    e4.prev = e3;
+    e5.next = e3;
+    e5.prev = e4;
+
+    
+    //twins
+    if(hasTwin(edgeList[i].prev)) {
+        e1.setTwin(edgeList[i].prev.twin);
+        edgeList[i].prev.twin.setTwin(e1);
+    }
+
+    if(hasTwin(edgeList[i])) {
+        if(hasTwin(edgeList[i].twin.next)) {
+            e2.setTwin(edgeList[i].twin.next.twin);
+            edgeList[i].twin.next.twin.setTwin(e2);
+        }
+        if(hasTwin(edgeList[i].twin.prev)) {
+            e4.setTwin(edgeList[i].twin.prev.twin);
+            edgeList[i].twin.prev.twin.setTwin(e4);
+        }
+    }
+
+    if(hasTwin(edgeList[i].next)) {
+        e5.setTwin(edgeList[i].next.twin);
+        edgeList[i].next.twin.setTwin(e5);
+    }
+
+    e0.setTwin(e3);
+    e3.setTwin(e0);
+
+
+    //push
+    edgeList[e0.id] = e0;
+    edgeList[e1.id] = e1;
+    edgeList[e2.id] = e2;
+    edgeList[e3.id] = e3;
+    edgeList[e4.id] = e4;
+    edgeList[e5.id] = e5;
+    
+    //remove old
+    for(let q = 0; q < edgeToRemove.length; ++q) {
+        delete edgeList[edgeToRemove[q]];
+    }
+    
+
+    /*
+    let e0 = edgeList[i];
+    let e1 = edgeList[i].next;
+    let e2 = edgeList[i].prev;
+    let e3 = edgeList[i].twin;
+    let e4 = edgeList[i].twin.next;
+    let e5 = edgeList[i].twin.prev;
+
+    let f0 = edgeList[i].face;
+    let f1 = edgeList[i].twin.face;
+
+    f0.edge = e0;
+    f1.edge = e3;
+
+    e4.face = f0;
+    e5.face = f1;
+    e1.face = f1;
+    e2.face = f0;
+    e0.face = f0;
+    e3.face = f1;
+
+    let p0 = e0.vertex;
+    let p1 = e1.vertex;
+    let p2 = e2.vertex;
+    let p3 = e5.vertex;
+
+    e0.vertex = p3;
+    e1.vertex = p2;
+    e2.vertex = p0;
+    e3.vertex = p2;
+    e4.vertex = p3;
+    e5.vertex = p1;
+
+    e0.setTwin(e3);
+    e3.setTwin(e0);
+
+    let e2Twin = e2.twin;
+
+    if(hasTwin(e4)) {
+        e2.setTwin(e4.twin);
+        e4.twin.setTwin(e2);
+    }
+    if(hasTwin(e5)) {
+        e4.setTwin(e5.twin);
+        e5.twin.setTwin(e4);
+    }
+    if(hasTwin(e1)) {
+        e5.setTwin(e1.twin);
+        e1.twin.setTwin(e5);
+    }
+    if(e2Twin) {
+        e1.setTwin(e2Twin);
+        e2Twin.setTwin(e1);
+    }
+    */
+
 }
+
+function hasTwin(edge){
+    return (edge.twin == null) ? false : true;
+}
+
+function copyVec3(inVec) {
+    console.log(inVec);
+    return new THREE.Vector3(inVec.x, inVec.y, inVec.z);
+}
+
 
 function generateMesh(faceList) {
 
     let geo = new THREE.Geometry();
 
-    for(let i = 0; i < faceList.length; ++i) {
-        geo.vertices.push(faceList[i].edge.vertex.point);
-        geo.vertices.push(faceList[i].edge.next.vertex.point);
-        geo.vertices.push(faceList[i].edge.prev.vertex.point);
+    let i = 0;
+    for(let key in faceList) {
+        geo.vertices.push(faceList[key].edge.vertex.point);
+        geo.vertices.push(faceList[key].edge.next.vertex.point);
+        geo.vertices.push(faceList[key].edge.prev.vertex.point);
 
         let offset = 3*i;
         let newFace = new THREE.Face3(offset, offset+1, offset+2);
+        let newBackFace = new THREE.Face3(offset, offset+2, offset+1);
+
+        //color
+        newFace.color.setRGB(faceList[key].color.r, faceList[key].color.g, faceList[key].color.b);
+        newBackFace.color.setRGB(faceList[key].color.r, faceList[key].color.g, faceList[key].color.b);
+
 
         geo.faces.push(newFace);
+        //geo.faces.push(newBackFace);
 
         geo.computeFaceNormals();
         geo.computeVertexNormals();
+        ++i;
     }
 
     return geo;
+}
+
+
+function clamp(min,max,val) {
+    return Math.min(Math.max(val,min),max);
 }
--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
-var camera, scene, renderer;
-var geometry, material;
-var pointGeometry, pointMaterial, wireframeMaterial;
+var camera, scene, renderer, controls;
+var geometry, material, mesh;
+var pointGeometry, pointMaterial, wireframeMaterial, colorMaterial;
 var points;
-var HE = [];
-var FL = [];
-var VL = [];
+var clock;
+var RPM = 0.2;
+var HE = {};
+var FL = {};
+var VL = {};
 
-initDelaunay(1,1,HE, VL, FL);
+initDelaunay(1, 1, 0.05, HE, VL, FL);
 init();
 animate();
 
 function init() {
+	clock = new THREE.Clock(true);
 
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 50 );
 	camera.position.z = 2;
+	controls = new THREE.OrbitControls(camera);
 
 	scene = new THREE.Scene();
 
@@ -25,37 +29,48 @@ function init() {
 
 	//generate points
 	wireframeMaterial = new THREE.MeshStandardMaterial({color: 0xffffff});
+	colorMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, vertexColors: THREE.FaceColors});
 	wireframeMaterial.wireframe = true;
 	
 	//generate new point
-	for(let i = 0; i < 5; ++i) {
+	for(let i = 0; i < 500; ++i) {
 		let point = samplePosition();
 		insertPoint(point,FL, HE, VL);
-		
-		let geo = generateMesh(FL);
-		let mesh = new THREE.Mesh(geo, wireframeMaterial);
-		scene.add(mesh);
 	}
+
+	let geo = generateMesh(FL);
+	mesh = new THREE.Mesh(geo, wireframeMaterial);
+
+	mesh.geometry.colorsNeedUpdate = true;
+	scene.add(mesh);
+
+	console.log(Object.keys(FL).length);
+	console.log(Object.keys(HE).length);
 	
 
-	//draw
+	
+	//draw points
 	pointGeometry = new THREE.Geometry();
 
-	for(let i = 0; i < HE.length; ++i) {
-		pointGeometry.vertices.push(HE[i].vertex.point);
+	for(let key in VL) {
+		pointGeometry.vertices.push(VL[key].point);
 	}
 
 	pointMaterial = new THREE.PointsMaterial( {color: 0xff0000, size: 0.05} );
 	points = new THREE.Points(pointGeometry, pointMaterial);
-	scene.add(points);
+	//scene.add(points);
+	
 
+	controls.update();
 }
 
 function animate() {
-
 	requestAnimationFrame( animate );
-
+	controls.update();
 	//stuff
+	let rotY = RPM * (Math.PI*2.0) * clock.getDelta();
+	mesh.rotation.y += rotY;
+	points.rotation.y  += rotY;
 
 	renderer.render( scene, camera );
 
